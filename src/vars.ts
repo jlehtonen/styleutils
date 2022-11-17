02dee0a40b6cad3d6eeed36f3bbb38037ec20d14
Jaakko Lehtonen <>
2022-11-17 23:34:38 +0200

    Add support for media queries in defineVars

--- src/vars.ts
+++ src/vars.ts
@@ -1,18 +1,34 @@
 import { uniqueHashGenerator } from "./hash.js";
 import { style } from "@vanilla-extract/css";
+import { selector } from "./style.js";
+
+type VarValue = string | number | Record<string, string | number>;
 
 const defineVars =
-  <T extends Record<string, any>>(vars: T) =>
-  (values: Partial<Record<keyof T, any>>) => {
-    const varsMap: Record<string, any> = {};
+  <T extends Record<string, string | number>>(vars: T) =>
+  (values: Partial<Record<keyof T, VarValue>>) => {
+    const styleObjects: any[] = [];
     Object.entries(values).forEach(([key, value]) => {
+      if (value === undefined) {
+        return;
+      }
       const varName = vars[key as keyof T];
-      varsMap[varName as any] = value;
+      if (typeof value === "string" || typeof value === "number") {
+        styleObjects.push({ vars: { [varName]: value } });
+      } else {
+        Object.entries(value).forEach(([subKey, subValue]) => {
+          styleObjects.push(
+            selector.screen(subKey)({
+              vars: {
+                [varName]: subValue.toString(),
+              },
+            })
+          );
+        });
+      }
     });
 
-    return style({
-      vars: varsMap,
-    });
+    return style(styleObjects);
   };
 
 export const createVars = <T extends Record<string, any>>(vars: T) => {
